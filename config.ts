17d9afad34b6ef2c89f571065d8d2d7e27f52659
tianxuejun <tianxuejun@noreply.gitcode.com>
2025-09-10 10:15:28 +0800

    feat(国际化): 替换默认租户时区
    --story=1018229 --user=田学军 【2508】【国际化】多时区 https://www.tapd.cn/23787791/s/2116751

--- config.ts
+++ config.ts
@@ -2,14 +2,14 @@
  * 时区配置接口
  */
 export interface ZoneConfig {
-  timezone: string;    // 默认时区
-  systemZone: string;  // 系统时区
+  timezone: string;    // 默认（个人）时区
+  systemZone: string;  // 系统（租户）时区
 }
 
 /**
  * 时区配置
  */
 export const zoneConfig: ZoneConfig = {
-  timezone: 'Asia/Shanghai',    // 默认时区
-  systemZone: 'Asia/Seoul'            // 系统时区
+  timezone: 'Asia/Shanghai',    // 默认（个人）时区
+  systemZone: 'Asia/Seoul'      // 系统（租户）时区
 };
