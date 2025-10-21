69a32d4242395a79c11252aebb27378225486266
tianxuejun <tianxuejun@noreply.gitcode.com>
2025-10-21 17:26:11 +0900

    feat(国际化): 设备时间不同，个人时区相同，字段值应该一致，现在不一致
    【灰度】【移动端】【多时区】设备时间不同，个人时区相同，字段值应该一致，现在不一致： https://devcloud.cn-north-4.huaweicloud.com/projectman/scrum/ea9762777b694cbe8b099d7f6acb453c/task/detail/70295547

--- implementations/DayjsTimeLibrary.ts
+++ implementations/DayjsTimeLibrary.ts
@@ -86,7 +86,7 @@ export class DayjsTimeLibrary extends ITimeLibrary {
         if (deviceTZ && deviceTZ != timezone) {
           const timeObj: TimeObject = this.lib(input);
           const currentOffset: number = timeObj.tz(timezone).utcOffset();
-          const systemOffset: number = timeObj.tz('Asia/Shanghai').utcOffset();
+          const systemOffset: number = timeObj.tz(deviceTZ).utcOffset();
           const offsetDiff: number = currentOffset - systemOffset;
           const timeStamp = timeObj.valueOf() - offsetDiff * 60 * 1000;
           return this.lib(timeStamp);
